0fd0b262bace2e595264bf29588e74c000e0a0bc
Sindre Sorhus <sindresorhus@gmail.com>
2018-05-30 15:05:54 +0700

    Init

---
+++ index.js
@@ -0,0 +1,89 @@
+'use strict';
+module.exports = {
+	parser: 'babel-eslint',
+	plugins: [
+		'flowtype'
+	],
+	rules: {
+		'flowtype/array-style-complex-type': [
+			'error',
+			'shorthand'
+		],
+		'flowtype/array-style-simple-type': [
+			'error',
+			'shorthand'
+		],
+		'flowtype/boolean-style': [
+			'error',
+			'boolean'
+		],
+		'flowtype/define-flow-type': 'error',
+		'flowtype/delimiter-dangle': [
+			'error',
+			'never'
+		],
+		'flowtype/generic-spacing': [
+			'error',
+			'never'
+		],
+		'flowtype/newline-after-flow-annotation': [
+			'error',
+			'never'
+		],
+		'flowtype/no-dupe-keys': 'error',
+		'flowtype/no-flow-fix-me-comments': 'warn',
+		'flowtype/no-primitive-constructor-types': 'error',
+		'flowtype/no-types-missing-file-annotation': 'error',
+		'flowtype/no-unused-expressions': 'error',
+		'flowtype/no-weak-types': [
+			'error',
+			{
+				any: false,
+				Object: true,
+				Function: true
+			}
+		],
+		'flowtype/object-type-delimiter': [
+			'error',
+			'comma'
+		],
+		'flowtype/require-parameter-type': 'error',
+		'flowtype/require-return-type': 'error',
+		'flowtype/require-types-at-top': [
+			'error',
+			'always'
+		],
+		'flowtype/require-valid-file-annotation': [
+			'error',
+			'always',
+			{
+				annotationStyle: 'line'
+			}
+		],
+		'flowtype/semi': [
+			'error',
+			'always'
+		],
+		'flowtype/space-after-type-colon': [
+			'error',
+			'always'
+		],
+		'flowtype/space-before-generic-bracket': [
+			'error',
+			'never'
+		],
+		'flowtype/space-before-type-colon': [
+			'error',
+			'never'
+		],
+		'flowtype/type-import-style': [
+			'error',
+			'identifier'
+		],
+		'flowtype/union-intersection-spacing': [
+			'error',
+			'always'
+		],
+		'flowtype/use-flow-type': 'error'
+	}
+};
---
+++ readme.md
@@ -0,0 +1,61 @@
+# eslint-config-xo-flow [![Build Status](https://travis-ci.org/xojs/eslint-config-xo-flow.svg?branch=master)](https://travis-ci.org/xojs/eslint-config-xo-flow)
+
+> ESLint [shareable config](http://eslint.org/docs/developer-guide/shareable-configs.html) for [Flow](https://flow.org) to be used with [eslint-config-xo](https://github.com/xojs/eslint-config-xo)
+
+
+## Install
+
+```
+$ npm install --save-dev eslint-config-xo eslint-config-xo-flow babel-eslint eslint-plugin-flowtype
+```
+
+
+## Usage
+
+Add some ESLint config to your package.json:
+
+```json
+{
+	"name": "my-awesome-project",
+	"eslintConfig": {
+		"extends": ["xo", "xo-flow"]
+	}
+}
+```
+
+Or to .eslintrc:
+
+```json
+{
+	"extends": ["xo", "xo-flow"]
+}
+```
+
+
+## Tip
+
+### Use with XO
+
+```
+$ npm install --save-dev eslint-config-xo-flow babel-eslint eslint-plugin-flowtype
+```
+
+```json
+{
+	"name": "my-awesome-project",
+	"xo": {
+		"extends": "xo-flow"
+	}
+}
+```
+
+
+## Related
+
+- [eslint-config-xo](https://github.com/xojs/eslint-config-xo) - ESLint shareable config for XO
+- [XO](https://github.com/xojs/xo)
+
+
+## License
+
+MIT © [Sindre Sorhus](https://sindresorhus.com)
